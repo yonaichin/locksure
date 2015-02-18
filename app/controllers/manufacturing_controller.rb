0a412713c61ff6c9472e5a81e70b97fafc78a37e
Anna Ho <annaho@ecoworkinc.com>
2015-02-18 12:32:48 +0800

    add manufacturing path

---
+++ app/controllers/manufacturing_controller.rb
@@ -0,0 +1,4 @@
+class ManufacturingController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :about, only: [:index]
   resources :products, only: [:index, :show]
   resources :contact, only: [:index, :create]
+  resources :manufacturing, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

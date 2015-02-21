59bfc2ab3ca947a3ee97d9fc02efa9cbbb551569
Anna Ho <annaho@ecoworkinc.com>
2015-02-22 00:36:01 +0800

    revise storage path

--- app/uploaders/category_uploader.rb
+++ app/uploaders/category_uploader.rb
@@ -2,7 +2,7 @@ class CategoryUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   def store_dir
-    'uploads/category'
+    'assets/uploads/category'
   end
   process :resize_to_fill => [358,239]
 end

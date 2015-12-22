d31bfd90d31cfcff33457599f631fce6f335e72a
Olly.W <yonaichin@gmail.com>
2015-12-22 12:50:48 +0800

    update contact page wording

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -5,15 +5,16 @@ jQuery(document).ready(function(){
         div: '#map',
         lat: 24.984672,
         lng: 121.539857,
+        zoom:16
     });
     map.addMarker({
         lat: 24.984672,
         lng: 121.539857,
-        title: 'Address',      
+        title: 'Address',
         infoWindow: {
             content: '<h5 class="title">今湛</h5><p><span class="region">8F, No.207, Sec.3, Beixin Rd., New Taipei City</span><br><span class="postal-code">231-43</span><br><span class="country-name">Taiwan</span></p>'
         }
-        
+
     });
 
 });

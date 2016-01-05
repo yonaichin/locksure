1c2feceac7241ebfea68ad6d3080269b9db199bd
Olly.W <yonaichin@gmail.com>
2016-01-05 10:52:55 +0800

    unique product image name

--- app/uploaders/product_uploader.rb
+++ app/uploaders/product_uploader.rb
@@ -4,5 +4,14 @@ class ProductUploader < CarrierWave::Uploader::Base
   def store_dir
     'assets/uploads/products'
   end
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+
+  end
   process :resize_to_fill => [1132,548]
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end

1c308fb3152e92950ffe5cae435f2cda572d76b6
kapiobhee <kapiobhee@gmail.com>
2016-01-08 04:07:39 +0800

    refactor catalog page

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -2,4 +2,8 @@ class CatalogController < ApplicationController
         def index
                 @catalog = Catalog.all
         end
+
+        def brochure
+          @catalog = Catalog.all
+        end
 end

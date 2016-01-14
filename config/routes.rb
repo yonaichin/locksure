619bd3e7a4b230b32a19c3a4fad64330082c67d9
kapiobhee <kapiobhee@gmail.com>
2016-01-14 11:35:38 +0800

    update about page -> about/introduction & about/companyfocus

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :home, only: [:index]
   root to: 'home#index'
-  resources :about, only: [:index]
   resources :products, only: [:index, :show]
   resources :contact, only: [:index, :create]
   get '/facilities/bolt/' => 'facilities#bolt'
@@ -11,4 +10,6 @@ Rails.application.routes.draw do
   get '/facilities/stamping/' => 'facilities#stamping'
   get '/catalog/index/' => 'catalog#index'
   get '/catalog/brochure/' => 'catalog#brochure'
+  get '/about/introduction/' => 'about#introduction'
+  get '/about/companyfocus/' => 'about#companyfocus'
 end

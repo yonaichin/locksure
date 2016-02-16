48a5fadb42a6d1bac42b2bcecbb1823ae42bb78e
kapiobhee <kapiobhee@gmail.com>
2016-02-16 14:39:28 +0800

    fix slides

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
     #@slides = Slide.all
-    @bolt_cid = Category.where(:name=> 'Bolt & Screw').first.id
-    @nut_cid = Category.where(:name=> 'Nut').first.id
-    @stamping_cid = Category.where(:name=> 'Stamping').first.id
+    @bolt_cid = Category.where(:name=> 'Bolt & Screw').first.id if Category.where(:name => 'Bolt & Screw').exists?
+    @nut_cid = Category.where(:name=> 'Nut').first.id if Category.where(:name => 'Nut').exists?
+    @stamping_cid = Category.where(:name=> 'Stamping').first.id if Category.where(:name => 'Stamping').exists?
     @bolt_img = Product.where(:category_id=> @bolt_cid).order('RAND()').limit(1)
     @nut_img = Product.where(:category_id=> @nut_cid).order('RAND()').limit(1)
     @stamping_img = Product.where(:category_id=> @stamping_cid).order('RAND()').limit(1)

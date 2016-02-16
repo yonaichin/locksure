5aa35834670f4511367c6a552cc4a349980d376e
kapiobhee <kapiobhee@gmail.com>
2016-02-16 13:49:53 +0800

    update slides

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
-
   def index
     #@slides = Slide.all
-    @slides = Product.order('RAND()').limit(8)
+    @bolt_cid = Category.where(:name=> 'Bolt & Screw').first.id
+    @nut_cid = Category.where(:name=> 'Nut').first.id
+    @stamping_cid = Category.where(:name=> 'Stamping').first.id
+    @bolt_img = Product.where(:category_id=> @bolt_cid).order('RAND()').limit(1)
+    @nut_img = Product.where(:category_id=> @nut_cid).order('RAND()').limit(1)
+    @stamping_img = Product.where(:category_id=> @stamping_cid).order('RAND()').limit(1)
+    @slides= @bolt_img + @nut_img + @stamping_img
   end
 end

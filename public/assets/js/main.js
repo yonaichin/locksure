cac953d6409a1763d017577abd8d5b5309981a1b
Olly.W <yonaichin@gmail.com>
2016-02-22 10:39:06 +0800

    hide slider index dot

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,18 +5,19 @@ jQuery(document).ready(function() {
     $('.flexslider').flexslider({
         animation: "fade"
     });
+    $('.slider ol').hide()
 
     /* ======= jQuery Placeholder ======= */
     $('input, textarea').placeholder();
-    
-    
+
+
     /* ======= Carousels ======= */
     $('#news-carousel').carousel({interval: false});
     $('#videos-carousel').carousel({interval: false});
     $('#testimonials-carousel').carousel({interval: 6000, pause: "hover"});
     $('#awards-carousel').carousel({interval: false});
-    
-    
+
+
     /* ======= Flickr PhotoStream ======= */
     $('#flickr-photos').jflickrfeed({
         limit: 12,
@@ -26,56 +27,56 @@ jQuery(document).ready(function() {
         itemTemplate:
             '<li>' +
             '<a rel="prettyPhoto[flickr]" href="{{image}}" title="{{title}}">' +
-            	'<img src="{{image_s}}" alt="{{title}}" />' +
+                '<img src="{{image_s}}" alt="{{title}}" />' +
             '</a>' +
             '</li>'
-			
+
     }, function(data) {
-    	$('#flickr-photos a').prettyPhoto();
+        $('#flickr-photos a').prettyPhoto();
     });
-    
+
     /* ======= Pretty Photo ======= */
-    // http://www.no-margin-for-errors.com/projects/prettyphoto-jquery-lightbox-clone/ 
+    // http://www.no-margin-for-errors.com/projects/prettyphoto-jquery-lightbox-clone/
     $('a.prettyphoto').prettyPhoto();
-    
+
     /* ======= Twitter Bootstrap hover dropdown ======= */
-    
+
     // apply dropdownHover to all elements with the data-hover="dropdown" attribute
     $('[data-hover="dropdown"]').dropdownHover();
-    
-    
+
+
     /* ======= Style Switcher ======= */
-    
+
     $('#config-trigger').on('click', function(e) {
         var $panel = $('#config-panel');
         var panelVisible = $('#config-panel').is(':visible');
         if (panelVisible) {
-            $panel.hide();;           
+            $panel.hide();;
         } else {
             $panel.show();
         }
         e.preventDefault();
     });
-    
+
     $('#config-close').on('click', function(e) {
         e.preventDefault();
         $('#config-panel').hide();
     });
-    
-    
-    $('#color-options a').on('click', function(e) { 
+
+
+    $('#color-options a').on('click', function(e) {
         var $styleSheet = $(this).attr('data-style');
         var $logoImage = $(this).attr('data-logo');
-		$('#theme-style').attr('href', $styleSheet);
-		$('#logo').attr('src', $logoImage);		
-				
-		var $listItem = $(this).closest('li');
-		$listItem.addClass('active');
-		$listItem.siblings().removeClass('active');
-		
-		e.preventDefault();
-		
-	});
+                $('#theme-style').attr('href', $styleSheet);
+                $('#logo').attr('src', $logoImage);
+
+                var $listItem = $(this).closest('li');
+                $listItem.addClass('active');
+                $listItem.siblings().removeClass('active');
+
+                e.preventDefault();
+
+        });
 
 
 });

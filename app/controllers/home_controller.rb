a6731c5b158d0e71f56b286bdb2d739280773bb0
Olly.W <yonaichin@gmail.com>
2016-02-22 10:31:07 +0800

    show all picture and hide slider index dot

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,9 @@ class HomeController < ApplicationController
     bolt_cid = Category.where(:name=> 'Bolt & Screw').first.id if Category.where(:name => 'Bolt & Screw').exists?
     nut_cid = Category.where(:name=> 'Nut').first.id if Category.where(:name => 'Nut').exists?
     stamping_cid = Category.where(:name=> 'Stamping').first.id if Category.where(:name => 'Stamping').exists?
-    @bolt_img = Product.where(:category_id=> bolt_cid).order('RAND()').limit(3)
-    @nut_img = Product.where(:category_id=> nut_cid).order('RAND()').limit(3)
-    @stamping_img = Product.where(:category_id=> stamping_cid).order('RAND()').limit(3)
+    @bolt_img = Product.where(:category_id=> bolt_cid).order('RAND()')
+    @nut_img = Product.where(:category_id=> nut_cid).order('RAND()')
+    @stamping_img = Product.where(:category_id=> stamping_cid).order('RAND()')
     # @slides= @bolt_img + @nut_img + @stamping_img
   end
 end
